11a0789340e947ba3f6b162af8cb245bd3e3a569
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-06-14 09:04:41 -0300

    refatoracao da CategoryController para trabalhar com Async

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,50 +1,53 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Models;
 
 //Representa o controller de categoria
 //https://localhost:5001/categories
+//realiza o parser via ModelBinder do JSON para o Objeto
+//desde o C# 6.0 foi incorporado o Async Await(Event loop) Task
 
 [Route("categories")]
 public class CategoryController : ControllerBase {
 
     [HttpGet]
     [Route("")]
-    public string Get()
+    public async Task<ActionResult<List<Category>>> Get()
     {
-        return "Olá mundo!";
+        return new List<Category>();
     }
 
     //exemplo de consulta por id com restrição
     [HttpGet]
     [Route("{id:int}")]
-    public string GetById(int id)
+    public async Task<ActionResult<Category>> GetById(int id)
     {
-        return "GET" + id.ToString();
+        return new Category();
     }
 
     [HttpPost]
     [Route("")]
-    public Category Post([FromBody] Category c)
+    public async Task<ActionResult<Category>> Post([FromBody] Category c)
     {
-        //realiza o parser via ModelBinder do JSON para o Objeto
-        return c;
+        return Ok(c);
     }
 
     [HttpPut]
     [Route("{id:int}")]
-    public Category Put(int id, [FromBody] Category c)
+    public async Task<ActionResult<Category>> Put(int id, [FromBody] Category c)
     {
         if(c.Id == id)
-        return c;
+        return Ok(c);
 
-        return null;
+        return NotFound();
     }
 
     [HttpDelete]
     [Route("")]
-    public string Delete()
+    public async Task<ActionResult<Category>> Delete()
     {
-        return "DELETE";
+        return Ok();
     }
 
 
