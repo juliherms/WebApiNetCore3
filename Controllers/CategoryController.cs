3146898c8d8c98505f4e5f7e32e4728550a7fba7
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-06-14 16:08:25 -0300

    realizando listagem e pesquisa de produtos

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -6,109 +6,112 @@ using Microsoft.EntityFrameworkCore;
 using Shop.Data;
 using Shop.Models;
 
-//Representa o controller de categoria
-//https://localhost:5001/categories
-//realiza o parser via ModelBinder do JSON para o Objeto
-//desde o C# 6.0 foi incorporado o Async Await(Event loop) Task
+namespace Shop.Controllers 
+{
+    //Representa o controller de categoria
+    //https://localhost:5001/categories
+    //realiza o parser via ModelBinder do JSON para o Objeto
+    //desde o C# 6.0 foi incorporado o Async Await(Event loop) Task
 
-[Route("categories")]
-public class CategoryController : ControllerBase {
+    [Route("categories")]
+    public class CategoryController : ControllerBase {
 
-    [HttpGet]
-    [Route("")]
-    public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context )
-    {
-        //retorna a lista de categorias
-        var cetegories = await context.Categories.AsNoTracking().ToListAsync();
-        return Ok(cetegories);
-    }
-
-    //exemplo de consulta por id com restrição
-    [HttpGet]
-    [Route("{id:int}")]
-    public async Task<ActionResult<Category>> GetById
-    (
-        int id,
-        [FromServices] DataContext context
-    )
-    {
-        var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
-        return Ok(category);
-    }
-
-    [HttpPost]
-    [Route("")]
-    public async Task<ActionResult<Category>> Post(
-        [FromBody] Category c,
-        [FromServices] DataContext context
-    )
-    {
-        //faz a validacao da categoria
-        if (!ModelState.IsValid)
-            return BadRequest(ModelState);
-        
-        try{
-
-            context.Categories.Add(c);
-            await context.SaveChangesAsync();
-            return Ok(c);
-
-        } catch 
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context )
         {
-            return BadRequest(new { message = "Não foi possível criar a categoria" });
+            //retorna a lista de categorias
+            var cetegories = await context.Categories.AsNoTracking().ToListAsync();
+            return Ok(cetegories);
         }
-        
-    }
 
-    [HttpPut]
-    [Route("{id:int}")]
-    public async Task<ActionResult<Category>> Put(
-        int id,
-        [FromBody] Category c,
-        [FromServices] DataContext context
-    )
-    {
-        //Verifica se o ID informado é o mesmo do Body
-        if(id != c.Id)
-            return NotFound(new { message = "Categoria não encontrada"});
+        //exemplo de consulta por id com restrição
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> GetById
+        (
+            int id,
+            [FromServices] DataContext context
+        )
+        {
+            var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(category);
+        }
 
-        //Verifica se o Body é válido
-        if(!ModelState.IsValid)
-            return BadRequest(ModelState);
-
-        try{
-            //faz a atualizacao otimizada - ele verifica propriedade por propriedade
-            context.Entry<Category>(c).State = EntityState.Modified;
-            await context.SaveChangesAsync();
-            return Ok(c);
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Category>> Post(
+            [FromBody] Category c,
+            [FromServices] DataContext context
+        )
+        {
+            //faz a validacao da categoria
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
             
-        } catch(DbUpdateConcurrencyException) {
-            return BadRequest(new { message = "Este registro já foi atualizado por outro processo"});
-        } catch(Exception) {
-            return BadRequest(new { message = "Não foi possível atualizar a categoria"});
+            try{
+
+                context.Categories.Add(c);
+                await context.SaveChangesAsync();
+                return Ok(c);
+
+            } catch 
+            {
+                return BadRequest(new { message = "Não foi possível criar a categoria" });
+            }
+            
         }
-    }
 
-    [HttpDelete]
-    [Route("{id:int}")]
-    public async Task<ActionResult<Category>> Delete(
-        int id,
-        [FromServices] DataContext context
-    )
-    {
-        //recupera a nossa categoria do banco de dados(pesquisa pelo id)
-        var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
-        if(category == null)
-            return NotFound(new { message = "Categoria não encontrada" });
-
-        try
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> Put(
+            int id,
+            [FromBody] Category c,
+            [FromServices] DataContext context
+        )
         {
-            context.Categories.Remove(category);
-            await context.SaveChangesAsync();
-            return Ok(new { message = "Categoria removida com sucesso" });
+            //Verifica se o ID informado é o mesmo do Body
+            if(id != c.Id)
+                return NotFound(new { message = "Categoria não encontrada"});
 
-        } catch (Exception) {
-            return BadRequest(new { message = "Não foi possível remover a categoria"});
+            //Verifica se o Body é válido
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try{
+                //faz a atualizacao otimizada - ele verifica propriedade por propriedade
+                context.Entry<Category>(c).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return Ok(c);
+                
+            } catch(DbUpdateConcurrencyException) {
+                return BadRequest(new { message = "Este registro já foi atualizado por outro processo"});
+            } catch(Exception) {
+                return BadRequest(new { message = "Não foi possível atualizar a categoria"});
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> Delete(
+            int id,
+            [FromServices] DataContext context
+        )
+        {
+            //recupera a nossa categoria do banco de dados(pesquisa pelo id)
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if(category == null)
+                return NotFound(new { message = "Categoria não encontrada" });
+
+            try
+            {
+                context.Categories.Remove(category);
+                await context.SaveChangesAsync();
+                return Ok(new { message = "Categoria removida com sucesso" });
+
+            } catch (Exception) {
+                return BadRequest(new { message = "Não foi possível remover a categoria"});
+            }
         }
     }
 }
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers 
+{
+    //controller responsavel por prover os servicos de produtos
+    [Route("products")]
+    public class ProductController : ControllerBase {
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            //lista os produtos incluindo a categoria (o include representa um join)
+            var products = await context.Products.Include( x => x.Category).AsNoTracking().ToListAsync();
+            return products;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> GetById([FromServices] DataContext context, int id){
+
+            var product = await context
+                .Products
+                .Include(x => x.Category )
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return product;
+        }
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext context, int id){
+
+            var products = await context
+                .Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .Where( x => x.CategoryId == id) //retorna todos os produtos de uma categoria especifica
+                .ToListAsync();
+
+            return products;
+        }
+    }
+
+}
+
+
